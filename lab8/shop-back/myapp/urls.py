16c02cf819d522d6019893ff261b8101880644c8
RavilKabirov <ravel.super@list.ru>
2026-03-30 19:22:28 +0500

    Lab 9

--- lab8/shop-back/myapp/urls.py
+++ lab8/shop-back/myapp/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-from .views import ProductList, CategoryList, ProductDetail, CategoryDetail, CategoryProducts
-
-
-urlpatterns = [
-    path('products/', ProductList.as_view(), name="products-list"),
-    path('products/<int:pk>/', ProductDetail.as_view(), name="product"),
-    path('categories/', CategoryList.as_view(), name="category-list"),
-    path('categories/<int:pk>/', CategoryDetail.as_view(), name="category"),
-    path('categories/<int:pk>/products', CategoryProducts.as_view(), name="category-products"),
-]
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+from .views import CategoryViewSet
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+urlpatterns = router.urls
